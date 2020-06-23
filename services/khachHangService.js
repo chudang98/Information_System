70e84a141daaccfdf04adb1ea2c5b1cbc98d073f
ngoclam121 <lamngoclam121@gmail.com>
2020-06-23 20:54:23 +0700

    add thongtinkh

--- services/khachHangService.js
+++ services/khachHangService.js
@@ -2,9 +2,10 @@ const KhachHang = require('../models/KhachHang');
 const Nguoi = require('../models/Nguoi');
 
 module.exports = {
-  luuKhachHang,
-  checkAccount,
-  loginAccount,
+    luuKhachHang,
+    checkAccount,
+    loginAccount,
+    getAllClient,
 };
 
 async function luuKhachHang() {}
@@ -12,14 +13,21 @@ async function luuKhachHang() {}
 async function checkAccount() {}
 
 async function loginAccount(username, password) {
-  if (_.isEmpty(username) || _.isEmpty(password)) return { status: 'wrong' };
-  const user = await KhachHang.findOne({ userName: username }).select(
-    '+password'
-  );
-  if (!user || !(await user.isCorrectPassword(password, user.password))) {
-    return { status: 'wrong' };
-  }
-  return {
-    status: 'true',
-  };
+    if (_.isEmpty(username) || _.isEmpty(password)) return { status: 'wrong' };
+    const user = await KhachHang.findOne({ userName: username }).select(
+        '+password'
+    );
+    if (!user || !(await user.isCorrectPassword(password, user.password))) {
+        return { status: 'wrong' };
+    }
+    return {
+        status: 'true',
+    };
+}
+
+async function getAllClient() {
+    var clients = await KhachHang
+        .find()
+        .populate('nguoi');
+    return clients;
 }
