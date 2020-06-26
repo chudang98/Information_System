f5eb91a6d52346b6aa65be824df76510eba09a7d
ngoclam121 <lamngoclam121@gmail.com>
2020-06-26 22:43:32 +0700

    fix seller

--- routers/nhanvienbanhangRouters.js
+++ routers/nhanvienbanhangRouters.js
@@ -9,7 +9,7 @@ router.route('/themkh').get(nvbhController.themKHview);
 router.route('/thongtinkh').get(nvbhController.thongtinKHview);
 router.route('/thongtinmh').get(nvbhController.thongtinMHview);
 router.route('/thongtincanhan').get(nvbhController.thongtinCNview);
-router.route('/xemchitiet').get(nvbhController.xemchitietview);
+router.route('/xemchitiet/:idkh').get(nvbhController.xemchitietview);
 router.route('/timkiemmh').get(nvbhController.timkiemMHview);
 router.route('/hoadon').get(nvbhController.hoaDonview);
 module.exports = router;
