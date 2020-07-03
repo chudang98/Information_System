c486e32188637b6bf2128b110af1896ba413a086
Duck <chudang98hn@gmail.com>
2020-07-03 16:37:53 +0700

    fix error danhgia

--- models/DanhGia.js
+++ models/DanhGia.js
@@ -1,12 +1,17 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const DanhGia = sequelize.define('DanhGia', {
-    nhanXet: DataTypes.STRING,
-    danhGia: {
-      type : DataTypes.INTEGER,
-      allowNull: true,
+    _id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
     },
-  }, {});
+    nhanXet: DataTypes.STRING,
+  }, {
+    freezeTableName: true,
+    tableName: 'DanhGias',
+  });
   DanhGia.associate = function(models) {
     // associations can be defined here
     DanhGia.belongsTo(models.KhachHang, {
@@ -17,6 +22,7 @@ module.exports = (sequelize, DataTypes) => {
     DanhGia.belongsTo(models.MatHang, {
       foreignKey: 'MatHangid',
       targetKey: '_id',
+      as: 'nguoi',
     })
   };
   return DanhGia;
